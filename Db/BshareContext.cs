05a1fa20d7f735015663e360ca8f1b4fd5125a72
beetron <85221688+beetron@users.noreply.github.com>
2023-09-07 21:50:20 +0900

    .

---
+++ Db/BshareContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bshare.Db
+{
+    public class BshareContext : DbContext
+    {
+        public BshareContext(DbContextOptions<BshareContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Models/File.cs
@@ -0,0 +1,15 @@
+﻿namespace Bshare.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? FileTitle { get; set; }
+        public string? FileDescription { get; set; }
+        public string? FilePath { get; set; }
+        public string? FileShortLink { get; set; }
+        DateTime UploadTime { get; set; }
+        DateTime ExpireTime { get; set; }
+
+    }
+}
