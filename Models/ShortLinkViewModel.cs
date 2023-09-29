05cb74651b98910cdabcca643bcb7b11790e3cda
Brian <bdev@btro.jp>
2023-09-29 19:02:43 +0900

    Delete ShortLinkViewModel.cs

--- Models/ShortLinkViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bshare.Models
-{
-    public class ShortLinkViewModel
-    {
-        public IEnumerable<FileUpload> FileUploads { get; set; }
-        public IEnumerable<FileDetail> FileDetails { get; set; }
-    }
-}
